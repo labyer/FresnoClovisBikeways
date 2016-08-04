3e2616800ba3908205ddc861125d5ffdd2b1c31a
Laura Byer <laura.a.byer@gmail.com>
2016-08-04 10:32:02 -0400

    commit

--- js/userconfig.js
+++ js/userconfig.js
@@ -4,7 +4,7 @@
 
 var config = {
 	// Set Map Bounds & point map is centered around
-	mapFocus : [36.766107,-119.736650],
+	mapFocus : [36.804736, -119.755339],
 	// Mapbox access token & key for basemap
 	mapboxAccessToken : 'pk.eyJ1IjoiYWx0YXBsYW5uaW5nIiwiYSI6InhqNzQwRW8ifQ.mlA6eN3JguZL_UkEV9WlMA',
     // username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
